23485f91c1c4326d6a514477a5ec45af80c7fc01
runic-tears <51153795+runic-tears@users.noreply.github.com>
2019-08-23 18:26:55 +0400

    :recycle: Refactoring

--- Twig/Utils/Configurator.py
+++ Twig/Utils/Configurator.py
@@ -5,8 +5,11 @@ try:
     # Подключение файла конфигурации
     __temp__ = open('./Config/master.yml', 'r', encoding='utf-8')
     # Загрузка данных из файла конфигурации
-    cfg = yaml.safe_load(__temp__)
-    cfg = cfg['CONFIG']
+    master = yaml.safe_load(__temp__)
+    cfg = master['CONFIG'.upper()]
+    whitelist = master['whitelist'.upper()]
+    IGNORED_CHANNELS = master['IGNORED_CHANNELS'.upper()]
+    INITIAL_COGS = master['INITIAL_COGS'.upper()]
     # Удаление временных данных
     __temp__.close()
     del __temp__
@@ -17,45 +20,5 @@ try:
     BOT_IS_NO_PERMS_MSG_ENABLED = cfg['SHOW_NO_PERMS_MESSAGES']
     MAIN_LOGS_CHANNEL = cfg['LOG_CHANNEL_ID']
     XP_LOGS_CHANNEL = cfg['XP_LOG_CHANNEL_ID']
-except Exception as err:
-    print(err)
-
-
-# Попытка получения белого списка
-try:
-    # Подключение файла конфигурации
-    __temp__ = open('./Config/master.yml', 'r', encoding='utf-8')
-    # Загрузка данных из файла конфигурации
-    whitelist = yaml.safe_load(__temp__)
-    whitelist = whitelist['whitelist'.upper()]
-    # Удаление временных данных
-    __temp__.close()
-    del __temp__
-except Exception as err:
-    print(err)
-
-# Попытка получения белого списка
-try:
-    # Подключение файла конфигурации
-    __temp__ = open('./Config/master.yml', 'r', encoding='utf-8')
-    # Загрузка данных из файла конфигурации
-    IGNORED_CHANNELS = yaml.safe_load(__temp__)
-    IGNORED_CHANNELS = IGNORED_CHANNELS['ignored_channels'.upper()]
-    # Удаление временных данных
-    __temp__.close()
-    del __temp__
-except Exception as err:
-    print(err)
-
-# Попытка получения списка модулей, включаемых при запуске бота
-try:
-    # Подключение файла конфигурации
-    __temp__ = open('./Config/master.yml', 'r', encoding='utf-8')
-    # Загрузка данных из файла конфигурации
-    INITIAL_COGS = yaml.safe_load(__temp__)
-    INITIAL_COGS = INITIAL_COGS['INITIAL_COGS'.upper()]
-    # Удаление временных данных
-    __temp__.close()
-    del __temp__
 except Exception as err:
     print(err)
