ab8d8ea7312e449377100330d82e3be8d59df250
runic-tears <51153795+runic-tears@users.noreply.github.com>
2019-09-21 23:07:40 +0400

    Кортежи быстрее, энта факт

--- Twig/TwigCore.py
+++ Twig/TwigCore.py
@@ -33,7 +33,7 @@ import unicodedata
 import lyricsgenius
 
 repo = git.Repo(".git")
-ARTISTS_BLACKLIST = ['face', 'фэйс', 'lida']
+ARTISTS_BLACKLIST = ('face', 'фэйс', 'lida')
 
 print('[CORE] Imports are ready...')
 
